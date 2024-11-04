da7212236384a1b2236c63c4359a2611194269bc
NotYair <yairtorres844@gmail.com>
2024-11-04 14:42:25 -0800

    updated the way command files are handled

--- index.js
+++ index.js
@@ -13,20 +13,16 @@ app.get('/', (req, res) => {
 });
 
 client.commands = new Collection();
-const folderPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(folderPath);
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const folder of commandFolders) {
-    const commandsPath = path.join(folderPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.log(`Command ${file} does not have a data property.`);
-        }
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`Command ${file} does not have a data property.`);
     }
 }
 
