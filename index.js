405b459e74a23930c5622696ca4599e52dcfece6
NotYair <yairtorres844@gmail.com>
2024-11-04 14:53:52 -0800

    defined it twice :sob:

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const http = require('node:http');
-const http = require('node:http');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const express = require('express');
 const { token } = process.env;
